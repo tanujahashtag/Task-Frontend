d63d989306d4eb5276604480082c8fde351c8f64
tanujahashtag <tanuja@hashtagtechno.com>
2025-04-26 17:16:09 +0400

    Fixes

--- src/app/components/image-crop/image-crop.component.ts
+++ src/app/components/image-crop/image-crop.component.ts
@@ -1,19 +1,19 @@
-import { Component } from '@angular/core';
-import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+// import { Component } from '@angular/core';
+// import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
-@Component({
-  selector: 'app-image-crop',
-  standalone: true,
-  imports: [],
-  templateUrl: './image-crop.component.html',
-  styleUrl: './image-crop.component.scss'
-})
-export class ImageCropComponent {
-  constructor(public activeModal: NgbActiveModal,){
+// @Component({
+//   selector: 'app-image-crop',
+//   standalone: true,
+//   imports: [],
+//   templateUrl: './image-crop.component.html',
+//   styleUrl: './image-crop.component.scss'
+// })
+// export class ImageCropComponent {
+//   constructor(public activeModal: NgbActiveModal,){
 
-  }
-  closeModal() {
-    this.activeModal.close();
-  }
-  uploadImage(){}
-}
+//   }
+//   closeModal() {
+//     this.activeModal.close();
+//   }
+//   uploadImage(){}
+// }
--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -5,7 +5,7 @@ import { CommonModule } from '@angular/common';
 import { TagInputModule } from 'ngx-chips';
 import { FormsModule } from '@angular/forms';
 import { NotificationService } from '../../services/notification.service';
-import { ImageCropComponent } from '../../components/image-crop/image-crop.component';
+// import { ImageCropComponent } from '../../components/image-crop/image-crop.component';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
@@ -27,31 +27,31 @@ export class ProfileComponent {
     const storedUser = localStorage.getItem('userInfo');
     this.userInfo = storedUser ? JSON.parse(storedUser) : null;
   }
-  onChangeFile(event: any) {
-    this.imageChangedEvent = event.target.files[0];
-    var extension = event.target.files[0]?.name.substr(
-      event.target.files[0]?.name.lastIndexOf('.')
-    );
-    var extnsn = extension.toLowerCase();
-    if (this.fileTypesAllowed.includes(extnsn)) {
-      const initialState = {
-        imageChangedEvent: this.imageChangedEvent,
-        imageType: this.imageType,
-      };
-      const modalRef = this.modalService.open(ImageCropComponent)
-        modalRef.componentInstance.event.subscribe((data: any) => {
-          if (data=='Profile updated successfully') {
+  // onChangeFile(event: any) {
+  //   this.imageChangedEvent = event.target.files[0];
+  //   var extension = event.target.files[0]?.name.substr(
+  //     event.target.files[0]?.name.lastIndexOf('.')
+  //   );
+  //   var extnsn = extension.toLowerCase();
+  //   if (this.fileTypesAllowed.includes(extnsn)) {
+  //     const initialState = {
+  //       imageChangedEvent: this.imageChangedEvent,
+  //       imageType: this.imageType,
+  //     };
+  //     const modalRef = this.modalService.open(ImageCropComponent)
+  //       modalRef.componentInstance.event.subscribe((data: any) => {
+  //         if (data=='Profile updated successfully') {
             
-            this.ngOnInit();
-          }
-        })
-      // this.imageFileName = event.target.files[0].name;
-      // this.image = event.target.files[0];
-    } else {
-      this._notificationSvc.error(
-        'Error',
-        'Can not upload file of this format'
-      );
-    }
+  //           this.ngOnInit();
+  //         }
+  //       })
+  //     // this.imageFileName = event.target.files[0].name;
+  //     // this.image = event.target.files[0];
+  //   } else {
+  //     this._notificationSvc.error(
+  //       'Error',
+  //       'Can not upload file of this format'
+  //     );
+    // }
   }
-}
+
